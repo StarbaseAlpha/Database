677564a69b32fded9867610e6a6bf8662ce8ea34
Mike Fuller <emailthemike@gmail.com>
2019-07-11 00:32:45 -0400

    added new tests

--- node/test.js
+++ node/test.js
@@ -4,6 +4,10 @@ const path = require('path');
 const database = require(__dirname + path.sep + 'database');
 const db = database(__dirname + path.sep + 'testdb1');
 
+let items = Array.from(new Uint8Array(1000)).map((val,idx)=>{
+  return {"key":"item" + idx, "value":{"timestamp":Date.now()}};
+});
+
 async function Test() {
   let key = "hello";
   let data = {"message":"Hello world"};
@@ -11,11 +15,12 @@ async function Test() {
   console.log(write);
   let read = await db.get(key);
   console.log(read);
+  console.log('importing items now...');
+  let imp = await db.importDB(items);
+  console.log(imp);
   let exp = await db.exportDB();
   console.log(exp);
-  let imp = await db.importDB(exp);
-  console.log(imp);
-  let list = await db.list();
+  let list = await db.list({"values":1,"limit":10,"gt":"item900", "lt":"item905"});
   console.log(list);
   let del = await db.del([key]);
   console.log(del);
